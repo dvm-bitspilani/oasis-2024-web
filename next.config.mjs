6b356dd6629a2d655c6209ebaa72a725392c641e
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-08-09 16:25:19 +0530

    build: configure next to use raw-loader for shader files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, options) => {
+        config.module.rules.push({
+            test: /\.(glsl|vs|fs|vert|frag)$/,
+            use: ['raw-loader'],
+        });
+        return config;
+    },
+};
 
 export default nextConfig;

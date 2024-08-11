877f2c3161dcf41773d8909c421277aed24574ec
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-08-11 16:53:07 +0530

    feat: fluid configs

--- components/Landing/Backdrop/FluidBackground.tsx
+++ components/Landing/Backdrop/FluidBackground.tsx
@@ -8,28 +8,28 @@ export default function FluidBackground() {
     const canvasRef = useRef(null)
     useEffect(() => {
         webGLFluidEnhanced.config({
-            SIM_RESOLUTION: 128, // Resolution of the simulation grid
+            SIM_RESOLUTION: 256, // Resolution of the simulation grid
             DYE_RESOLUTION: 1024, // Resolution of the dye grid
             CAPTURE_RESOLUTION: 512, // Resolution of captured frames
             DENSITY_DISSIPATION: 1, // Rate at which density dissipates
             VELOCITY_DISSIPATION: 0.2, // Rate at which velocity dissipates
             PRESSURE: 0.8, // Pressure value used in the simulation
-            PRESSURE_ITERATIONS: 10, // Number of pressure iterations
-            CURL: 30, // Curl value used in the simulation
+            PRESSURE_ITERATIONS: 20, // Number of pressure iterations
+            CURL: 20, // Curl value used in the simulation
             INITIAL: false, // Enables splats on initial load
             SPLAT_AMOUNT: 5, // Number of initial splats (Random number between n and n * 5)
-            SPLAT_RADIUS: 0.3, // Radius of the splats
+            SPLAT_RADIUS: 0.25, // Radius of the splats
             SPLAT_FORCE: 6000, // Force applied by the splats
             SPLAT_KEY: '', // Keyboard key to spawn new splats (empty to disable)
             SHADING: true, // Enables shading in the visualization
             COLORFUL: true, // Enables rapid changing of colors
             COLOR_UPDATE_SPEED: 10, // Speed of color update
-            COLOR_PALETTE: [], // Custom color palette (empty by default, uses hex colors)
+            COLOR_PALETTE: ['#FF69B4'], // Custom color palette (empty by default, uses hex colors)
             HOVER: true, // Enables interaction on hover
             BACK_COLOR: '#190D2C', // Background color of the canvas
             TRANSPARENT: false, // Makes the canvas transparent if true
             BRIGHTNESS: 0.5, // Color brightness (Recommend lower than 1.0 if BLOOM is true)
-            BLOOM: true, // Enables bloom effect
+            BLOOM: false, // Enables bloom effect
             BLOOM_ITERATIONS: 8, // Number of bloom effect iterations
             BLOOM_RESOLUTION: 256, // Resolution of the bloom effect
             BLOOM_INTENSITY: 0.8, // Intensity of the bloom effect

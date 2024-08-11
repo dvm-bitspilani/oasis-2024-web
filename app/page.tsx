93ef53dfe7cf18323a5c9abd263e5eb0826273ed
Samyak Jain <jainsamyak1203@gmail.com>
2024-08-11 14:14:51 +0530

    feat: added machine trees and cards

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import Test from "@/components/TestComponent";
 import LandingBackdrop from '@/components/Landing/Backdrop/Backdrop';
 // import FluidBackground from '@/components/Landing/Backdrop/FluidBackground';
 import FluidBackground from '@/components/Landing/Backdrop/FluidBackground';
+import LandingPage from '@/components/Landing/LandingPage/LandingPage';
+import CardsGroup from '@/components/Landing/CardsGroup/CardsGroup';
 
 export default function Home() {
   return (
@@ -11,6 +13,7 @@ export default function Home() {
       <h1 className={styles.mainTitle}>Oasis 2024</h1>
       {/* <LandingBackdrop /> */}
       <FluidBackground />
+      {/* <LandingPage /> */}
     </>
   );
 }

712519b1e98c822a7729a63ef363722209833c94
Chaitanya Jain <175200580+chaitanya-rahul-jain@users.noreply.github.com>
2024-08-14 19:34:15 +0530

    style: registration form styling

--- components/Registration/RegistrationForm/RegistrationForm.tsx
+++ components/Registration/RegistrationForm/RegistrationForm.tsx
@@ -6,6 +6,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 
 import styles from "./registrationForm.module.scss";
+import Image from "next/image";
 
 const formSchema = z.object({
   name: z.string().min(1, { message: "*Name is required" }),
@@ -56,6 +57,30 @@ const RegistrationForm: React.FC = () => {
           NAME
         </label>
         <input id="name" {...register("name")} />
+        <div className={styles.inputUnderline}>
+          <svg
+            width="904"
+            height="33"
+            viewBox="0 0 904 33"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M888.293 22.3075C895.265 19.5085 892.004 13.6209 889.118 12.2607C885.66 10.6306 875.637 12.2767 875.834 23.5311C877.338 32.6431 895.434 38.06 902.57 19.5421C905.43 11.2526 898.547 0.107649 882.739 1.05681H0"
+              stroke="#F5E3AE"
+              stroke-width="1.58383"
+            />
+            <line
+              x1="-7.14315e-08"
+              y1="9.18292"
+              x2="875"
+              y2="9.18284"
+              stroke="#F5E3AE"
+              stroke-width="1.63416"
+              stroke-dasharray="1.63 1.63"
+            />
+          </svg>
+        </div>
         {errors.name && (
           <span className={styles.formErrorMessage}>{errors.name.message}</span>
         )}
@@ -66,6 +91,30 @@ const RegistrationForm: React.FC = () => {
           EMAIL ID
         </label>
         <input id="email" type="email" {...register("email")} />
+        <div className={styles.inputUnderline}>
+          <svg
+            width="904"
+            height="33"
+            viewBox="0 0 904 33"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M888.293 22.3075C895.265 19.5085 892.004 13.6209 889.118 12.2607C885.66 10.6306 875.637 12.2767 875.834 23.5311C877.338 32.6431 895.434 38.06 902.57 19.5421C905.43 11.2526 898.547 0.107649 882.739 1.05681H0"
+              stroke="#F5E3AE"
+              stroke-width="1.58383"
+            />
+            <line
+              x1="-7.14315e-08"
+              y1="9.18292"
+              x2="875"
+              y2="9.18284"
+              stroke="#F5E3AE"
+              stroke-width="1.63416"
+              stroke-dasharray="1.63 1.63"
+            />
+          </svg>
+        </div>
         {errors.email && (
           <span className={styles.formErrorMessage}>
             {errors.email.message}
@@ -78,6 +127,30 @@ const RegistrationForm: React.FC = () => {
           PHONE NUMBER
         </label>
         <input id="phoneNumber" type="tel" {...register("phoneNumber")} />
+        <div className={styles.inputUnderline}>
+          <svg
+            width="904"
+            height="33"
+            viewBox="0 0 904 33"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M888.293 22.3075C895.265 19.5085 892.004 13.6209 889.118 12.2607C885.66 10.6306 875.637 12.2767 875.834 23.5311C877.338 32.6431 895.434 38.06 902.57 19.5421C905.43 11.2526 898.547 0.107649 882.739 1.05681H0"
+              stroke="#F5E3AE"
+              stroke-width="1.58383"
+            />
+            <line
+              x1="-7.14315e-08"
+              y1="9.18292"
+              x2="875"
+              y2="9.18284"
+              stroke="#F5E3AE"
+              stroke-width="1.63416"
+              stroke-dasharray="1.63 1.63"
+            />
+          </svg>
+        </div>
         {errors.phoneNumber && (
           <span className={styles.formErrorMessage}>
             {errors.phoneNumber.message}
@@ -85,18 +158,33 @@ const RegistrationForm: React.FC = () => {
         )}
       </div>
 
-      <div className={styles.formField}>
+      <div className={`${styles.formField} ${styles.radioInput}`}>
         <label className={styles.formFieldHeader}>GENDER</label>
-        <div>
-          <input type="radio" id="male" value="MALE" {...register("gender")} />
-          <label htmlFor="male">Male</label>
-          <input
-            type="radio"
-            id="female"
-            value="FEMALE"
-            {...register("gender")}
-          />
-          <label htmlFor="female">Female</label>
+        <div className={styles.radioContainer}>
+          <div className={styles.radioButtonContainer}>
+            <input
+              type="radio"
+              id="male"
+              value="MALE"
+              {...register("gender")}
+            />
+            <label htmlFor="male" className={styles.radioLabel}>
+              Male
+            </label>
+          </div>
+
+          <div className={styles.radioButtonContainer}>
+            <input
+              type="radio"
+              id="female"
+              value="FEMALE"
+              {...register("gender")}
+            />
+            <label htmlFor="female" className={styles.radioLabel}>
+              Female
+            </label>
+          </div>
+
           {/* <input
             type="radio"
             id="others"
@@ -159,19 +247,21 @@ const RegistrationForm: React.FC = () => {
         )}
       </div>
 
-      <div className={styles.formField}>
+      <div className={`${styles.formField} ${styles.radioInput}`}>
         <label className={styles.formFieldHeader}>YEAR OF STUDY</label>
-        <div>
+        <div className={styles.radioContainer}>
           {[1, 2, 3, 4, 5].map((year) => (
-            <React.Fragment key={year}>
+            <div key={year} className={styles.radioButtonContainer}>
               <input
                 type="radio"
                 id={`year${year}`}
                 value={year.toString()}
                 {...register("yearOfStudy")}
               />
-              <label htmlFor={`year${year}`}>{year}</label>
-            </React.Fragment>
+              <label htmlFor={`year${year}`} className={styles.radioLabel}>
+                {year}
+              </label>
+            </div>
           ))}
         </div>
         {errors.yearOfStudy && (

7cccb4ec2fa8bfdc0bdd2f3bef396a651ccd4fc1
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-08-14 17:44:38 +0530

    perf: monitoring performance

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@ import styles from './landing.module.scss'
 import LandingBackdrop from '@/components/Landing/Backdrop/Backdrop';
 import Grunge from '@/components/Landing/Backdrop/Grunge';
 import LandingPage from '@/components/Landing/LandingPage/LandingPage';
+import LandingScene from '@/components/Landing/Scene/Scene';
 
 export default function Home() {
   return (
     <main className={styles.page}>
-      <LandingBackdrop />
+      {/* <LandingBackdrop /> */}
+      <LandingScene />
       <div className={styles.overlay}>
         <Grunge />
         {/* <h1 className={styles.mainTitle}>Oasis 2024</h1> */}

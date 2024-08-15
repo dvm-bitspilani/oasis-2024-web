adf7591c86a2e58a927c9d03b6feaf944b6ac432
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-08-15 20:44:31 +0530

    style: text styling

--- components/AboutUs/AboutUsPage.tsx
+++ components/AboutUs/AboutUsPage.tsx
@@ -5,12 +5,8 @@ export default function AboutUsPage() {
     return (
         <main className={styles.aboutUs} id='aboutUs'>
             <AboutUsTitle />
-            <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quos quibusdam sit itaque ducimus quo dolorum placeat sequi quidem ullam illum quod voluptatem beatae error, eos totam pariatur nihil iure?
-                Eos molestias dolorem quasi minima ducimus incidunt excepturi accusamus doloribus! Dicta neque, quasi odio id, possimus doloremque nam suscipit dolorem, consequuntur ex at delectus repudiandae! Mollitia harum atque omnis inventore.
-                Laudantium optio quibusdam sequi quaerat laborum soluta natus? Dolorum commodi in possimus nam. Exercitationem nostrum harum maxime sapiente aut quasi quam est quidem odio earum modi suscipit, animi sint recusandae?
-                Deserunt aspernatur a, blanditiis rem pariatur fugiat consectetur dolorem. Impedit optio modi ullam nam nemo accusantium, quae porro aspernatur, exercitationem cumque aliquam molestias excepturi pariatur in magnam quidem a atque.
-                Quaerat fugit officia ullam autem molestias fuga alias porro voluptatum sapiente doloribus facilis dolor, accusantium, voluptatem dicta mollitia, debitis eaque ad laudantium delectus. Nisi error ea architecto itaque quod nemo!
-                Tempore officiis facilis debitis minus? Perspiciatis iste alias totam minus odio molestiae quidem tempore magni accusantium quasi maxime debitis soluta voluptatem suscipit, reprehenderit unde nihil distinctio laudantium, ratione aut amet.</p>
+            <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, quibusdam dicta minima possimus asperiores quaerat molestiae nesciunt error eligendi dolores ducimus nobis nisi impedit ullam pariatur nihil expedita suscipit culpa.
+                Error odit, sapiente voluptatum harum quod molestiae! Officia, distinctio tempore, sapiente maiores, neque iusto cum necessitatibus asperiores doloribus fuga repudiandae? Voluptatibus placeat laudantium esse, excepturi itaque error quae neque officia!</p>
         </main>
     )
 }

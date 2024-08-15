4b28deb722c5bd7a6635e8a023c96820c1d605ab
Samyak Jain <jainsamyak1203@gmail.com>
2024-08-15 14:15:42 +0530

    add register button without animation

--- components/Landing/Navbar/Navbar.tsx
+++ components/Landing/Navbar/Navbar.tsx
@@ -3,24 +3,92 @@ import styles from "./navbar.module.scss";
 import Image from "next/image";
 
 export default function Navbar() {
-    return (
-        <>
-            <div className={styles.header}>
-                <Image
-                    src="/hammenu.png"
-                    alt=""
-                    width={99}
-                    height={99}
-                />
+  return (
+    <>
+      <div className={styles.header}>
+        <Image
+          src="/hammenu.png"
+          alt=""
+          width={99}
+          height={99}
+          className={styles.hamburger}
+        />
+        <div className={styles.btnwrapper}>
+          <div className={styles.btnborder}>
+            <div className={`${styles.circlewrapper} ${styles.top}`}>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
             </div>
-            <div className={styles.logo}>
-                <Image
-                    src="/oasislogo.png"
-                    alt=""
-                    width={560}
-                    height={205}
-                />
+            <div className={`${styles.circlewrapper} ${styles.bottom}`}>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
             </div>
-        </>
-    )
+            <div className={`${styles.circlewrapper} ${styles.curve}`}>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+            </div>
+            <div className={`${styles.circlewrapper} ${styles.curve} ${styles.left}`}>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+              <div className={styles.circle}></div>
+            </div>
+            <div className={styles.register}>register</div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.logo}>
+        <Image src="/oasislogo.png" alt="" width={560} height={205} />
+      </div>
+    </>
+  );
 }

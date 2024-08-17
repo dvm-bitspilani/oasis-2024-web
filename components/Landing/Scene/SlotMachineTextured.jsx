89523c4fd3171f16e893c17578cc78faeaa6546a
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-08-17 15:24:14 +0530

    fix: fixed asset uv map

--- components/Landing/Scene/SlotMachineTextured.jsx
+++ components/Landing/Scene/SlotMachineTextured.jsx
@@ -7,9 +7,10 @@ import { useGLTF, useScroll, useVideoTexture } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
 import gsap from 'gsap'
 import { MeshTransmissionMaterial } from './MeshTransmssionMaterial'
+import { MeshBasicMaterial } from 'three'
 
 export function SlotMachine2(props) {
-    const { nodes, materials } = useGLTF('/Models/slotMachineSplitMeshBtn.glb')
+    const { nodes, materials } = useGLTF('/Models/slotMachine.glb')
 
     const tl = useRef()
     const slotMachine = useRef()
@@ -44,7 +45,7 @@ export function SlotMachine2(props) {
             material.transparent = false;
             material.opacity = 1;
             // material.color = new THREE.Color(0x9AF0F4);
-            screen.current.material = material;
+            screen.current.material.map = texture;
         }
 
         const videoElement = texture.source.data;
@@ -102,8 +103,8 @@ export function SlotMachine2(props) {
                 castShadow
                 receiveShadow
                 geometry={nodes.Sphere002.geometry}
-                // material={materials.MapTest}
-                ref={screen}
+                material={materials.MapTest}
+                position={[0.008, 0.317, -0.235]}
             />
             <mesh
                 castShadow
@@ -127,4 +128,4 @@ export function SlotMachine2(props) {
     )
 }
 
-useGLTF.preload('/Models/slotMachineSplitMeshBtn.glb')
+useGLTF.preload('/Models/slotMachine.glb')

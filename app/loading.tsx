97f64c467cf4e4d02a5ffd269bd80a5c5a441a81
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-08-21 17:03:04 +0530

    feat: Add Suspense with Preloader to Home component
    - Wrapped the Home component's main content with React's Suspense component to handle lazy loading.
    - Introduced a Preloader component as a fallback to display while the main content is loading.
    - Updated `preloader.module.scss` to include a high z-index for the Preloader to ensure it appears above other elements.

--- app/loading.tsx
+++
@@ -1,5 +0,0 @@
-import Preloader from "@/components/Preloader/Preloader";
-
-export default function Loading() {
-  return <Preloader />;
-}
--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,23 @@ import Trees from "@/components/Landing/Trees/Trees";
 import LandingOverlay from "@/components/Landing/LandingOverlay/LandingOverlay";
 import Countdown from "@/components/Landing/Countdown/Countdown";
 import Grid from "@/components/Landing/Grid/Grid";
+import { Suspense } from "react";
+import Preloader from "@/components/Preloader/Preloader";
 
 export default function Home() {
   return (
-    <main className={styles.scrollWrapper}>
-      <main className={styles.page}>
-        <Landing />
-        <LandingOverlay />
-        <Countdown dateString="October 23, 2024 19:00:00" />
-        <Grid />
-        <Glow />
-        <Trees />
-        <Grunge />
+    <Suspense fallback={<Preloader />}>
+      <main className={styles.scrollWrapper}>
+        <main className={styles.page}>
+          <Landing />
+          <LandingOverlay />
+          <Countdown dateString="October 23, 2024 19:00:00" />
+          <Grid />
+          <Glow />
+          <Trees />
+          <Grunge />
+        </main>
       </main>
-    </main>
+    </Suspense>
   );
 }

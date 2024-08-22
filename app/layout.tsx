ce32a20a6bd4e0ea77f61f36467ca7c59772ccc2
Chaitanya Jain <175200580+chaitanya-rahul-jain@users.noreply.github.com>
2024-08-22 19:54:55 +0530

    fix: remove next auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { SessionProvider } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <SessionProvider>{children}</SessionProvider>
+        {children}
       </body>
     </html>
   );

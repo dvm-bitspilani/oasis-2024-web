779a8bcbf3c3a2865a592d3bf15c2c5c575456e9
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-08-24 03:09:29 +0530

    style: instruction styles

--- components/Registration/SignIn/SignIn.tsx
+++ components/Registration/SignIn/SignIn.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./signin.module.scss";
-import Instruction from "../Instructions/instructions";
+import Instruction from "../Instructions/Instructions";
 
 const Signin = () => {
   return (

28338879ae32ff2905cae35641aca1ba6fa26acb
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-08-27 00:51:32 +0530

    iframe

---
+++ components/IFrameModal/Modal.tsx
@@ -0,0 +1,9 @@
+import styles from "./modal.module.scss";
+
+export default function Modal() {
+  return (
+    <dialog>
+      <iframe src="https://www.youtube.com/embed/ZCrClSBM1ns" />
+    </dialog>
+  );
+}

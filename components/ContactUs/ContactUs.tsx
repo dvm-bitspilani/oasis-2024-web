c153c46ffe76ae1c2d064b3b007fa19fd0de846b
Manas <manasck14@gmail.com>
2024-08-30 19:18:21 +0530

    "feat: added mobile view for contact-page "

--- components/ContactUs/ContactUs.tsx
+++ components/ContactUs/ContactUs.tsx
@@ -421,7 +421,6 @@ export default function ContactUs() {
           </div>
         </div>
 
-
         <div className={styles.cardContainer1} id="contactCard1">
 
           <div className={`${styles.first} card`}>
@@ -459,6 +458,84 @@ export default function ContactUs() {
               mail={null}
             />
           </div>
+
+        </div>
+
+        <div className={styles.mobileContainer}>
+
+          <ContactCard
+            name="Aryan Wadhwa"
+            img={aryan}
+            dept="Registration, Events & Approval Queries"
+            contact="tel:+919810297034"
+            mail="mailto:pcr@bits-oasis.org"
+          />
+
+          <ContactCard
+            name="Prateek Kashyap"
+            img={prateek}
+            dept="Website, App and Online Payments"
+            contact="tel:+918700225449"
+            mail="mailto:webmaster@bits-oasis.org"
+          />
+
+          <ContactCard
+            name="Shreeram Verma"
+            img={shreeram}
+            dept="Sponsorship and Marketing"
+            contact="tel:+918889588288"
+            mail="mailto:shreeram@bits-oasis.org"
+          />
+
+          <ContactCard
+            name="Jaiditya Singh"
+            img={jaiditya}
+            dept="Logistics and Operations"
+            // contact="tel:+919168421199"
+            contact={null}
+            // mail="mailto:controls@bits-oasis.org"
+            mail={null}
+          />
+
+          <ContactCard
+            name="Rijul Bassamboo"
+            img={rijul}
+            dept="Reception and Accommodation"
+            // contact="tel:+919811529877"
+            contact={null}
+            // mail="mailto:recnacc@bits-oasis.org"
+            mail={null}
+          />
+
+          <ContactCard
+            name="Shreyansh Vanjani"
+            img={shreyansh}
+            dept="Publicity and Collaborations"
+            //contact="tel:+918000695988"
+            contact={null}
+            // mail="mailto:collaborations@bits-oasis.org"
+            mail={null}
+          />
+
+          <ContactCard
+            name="Sarthak Aggarwal"
+            img={sarthak}
+            dept="President, Students' Union"
+            // contact="tel:+9180050 00213"
+            contact={null}
+            // mail="mailto:president@pilani.bits-pilani.ac.in"
+            mail={null}
+          />
+
+          <ContactCard
+            name="Abhinav Lamba"
+            img={abhinav}
+            dept="General Secretary, Students' Union"
+            // contact="tel:+917078879443"
+            contact={null}
+            // mail="mailto:gensec@pilani.bits-pilani.ac.in"
+            mail={null}
+          />
 
         </div>
       </div>

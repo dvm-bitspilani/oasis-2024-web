fbc7fe61b4f441cadb82a25c1130db8fb3c641cc
Manas <manasck14@gmail.com>
2024-08-30 18:37:53 +0530

    feat: added contact-page animation

--- components/Landing/Landing/Landing.tsx
+++ components/Landing/Landing/Landing.tsx
@@ -9,7 +9,7 @@ import ScrollTrigger from "gsap/dist/ScrollTrigger";
 import dynamic from "next/dynamic";
 
 import LandingScene from "../Scene/Scene";
-import styles from "../../ContactUs/contactus.module.scss";
+// import styles from "../../ContactUs/contactus.module.scss";
 import Preloader from "@/components/Preloader/Preloader";
 // const LandingScene = dynamic(() => import('../Scene/Scene'), { ssr: false })
 
@@ -230,8 +230,135 @@ export default function Landing() {
                 "#contactUs",
                 {
                   opacity: 1,
-                  duration: 1,
+                  duration: 0,
                   pointerEvents: "auto",
+                  onComplete: () => {
+                    const container = document.querySelector("#contactCard") as HTMLElement;
+                    const cards = container?.querySelectorAll(".card") as NodeListOf<HTMLElement>;
+                    if (cards) {
+                      const cardCount = cards.length;
+                      const containerWidth = container.offsetWidth;
+                      const cardWidth = cards[1]?.offsetWidth || 0;
+
+                      const X1 = (containerWidth - 5 * cardWidth - 215) / 2;
+                      const X2 = X1 + cardWidth + 50;
+                      const X3 = X2 + cardWidth + 50;
+                      const X4 = X3 + cardWidth + 50;
+                      const X5 = X4 + cardWidth + 50;
+
+                      const translations = [
+                        { x: X1, y: 70, rotation: -18 },
+                        { x: X2, y: -10, rotation: -10 },
+                        { x: X3, y: -35, rotation: 0 },
+                        { x: X4, y: -10, rotation: 10 },
+                        { x: X5, y: 70, rotation: 18 },
+                      ];
+
+                      gsap.set(cards, {
+                        x: X1,
+                        y: 70,
+                        rotation: -18,
+                        zIndex: (index) => index,
+                        duration: 0,
+                      });
+
+                      const tl = gsap.timeline({ delay: 1 });
+
+                      for (let i = 1; i < cardCount; i++) {
+                        tl.to(cards[i], {
+                          x: translations[i].x,
+                          y: translations[i].y,
+                          rotation: translations[i].rotation,
+                          zIndex: cardCount - i,
+                          transformOrigin: "center center",
+                          duration: 0.2,
+                          ease: "power1.inOut",
+                          onStart: () => {
+                            for (let j = i + 1; j < cardCount; j++) {
+                              gsap.to(cards[j], {
+                                x: translations[i].x,
+                                y: translations[i].y,
+                                rotation: translations[i].rotation,
+                                transformOrigin: "center center",
+                                duration: 0.2,
+                                ease: "power1.inOut",
+                                zIndex: j,
+                              });
+                            }
+                          },
+                        });
+                      }
+
+                      const container1 = document.querySelector("#contactCard1") as HTMLElement;
+                      const cards1 = container1?.querySelectorAll(".card") as NodeListOf<HTMLElement>;
+                      if (cards1) {
+                        const cardCount1 = cards1.length;
+                        const cardHeight = cards[0]?.offsetHeight || 0;
+
+                        const X6 = (containerWidth - 3 * cardWidth - 105) / 2;
+                        const X7 = X6 + cardWidth + 50;
+                        const X8 = X7 + cardWidth + 50;
+
+                        const Y6 = cardHeight ;
+                        const Y7 = cardHeight - 30;
+                        const Y8 = cardHeight ;
+
+                        const translations1 = [
+                          { x: X6, y: Y6, rotation: -12 },
+                          { x: X7, y: Y7, rotation: 0 },
+                          { x: X8, y: Y8, rotation: 12 },
+                        ];
+
+                        gsap.set(cards1, {
+                          opacity: 0,
+                          x: X6,
+                          y: Y6,
+                          rotation: -12,
+                          zIndex: (index) => index,
+                          duration: 0,
+                        });
+
+                        const tl1 = gsap.timeline({
+                          paused: true,
+                          onStart: () => {
+                            gsap.to(cards1, { opacity: 1, duration: 0.5 });
+                          },
+                        });
+
+                        for (let i = 0; i < cardCount1; i++) {
+                          tl1.to(cards1[i], {
+                            x: translations1[i].x,
+                            y: translations1[i].y,
+                            rotation: translations1[i].rotation,
+                            zIndex: cardCount1 - i,
+                            transformOrigin: "center center",
+                            duration: 0.2,
+                            ease: "power1.out",
+                            onStart: () => {
+                              for (let j = i + 1; j < cardCount1; j++) {
+                                gsap.to(cards1[j], {
+                                  x: translations1[i].x,
+                                  y: translations1[i].y,
+                                  rotation: translations1[i].rotation,
+                                  transformOrigin: "center center",
+                                  duration: 0.2,
+                                  ease: "power1.out",
+                                  zIndex: j,
+                                });
+                              }
+                            },
+                          });
+                        }
+
+                        const masterTimeline = gsap.timeline();
+                        masterTimeline.add(tl);
+                        masterTimeline.add(() => {
+                          tl1.play();
+                          return null;
+                        });
+                      }
+                    }
+                  },
                 },
                 "-=1.5"
               )
@@ -242,33 +369,33 @@ export default function Landing() {
                   duration: 1,
                 },
                 "<"
-              )
-              .to(
-                {},
-                {
-                  onComplete: () => {
-                    const contactCard = document.querySelector("#contactCard");
-                    if (contactCard) {
-                      contactCard.classList.add(styles.active);
-                      // console.log(document.querySelector('#contactCard'));
-                    }
-                  },
-                },
-                "<"
-              )
-              .to(
-                {},
-                {
-                  onComplete: () => {
-                    const contactCard1 =
-                      document.querySelector("#contactCard1");
-                    if (contactCard1) {
-                      contactCard1.classList.add(styles.active1);
-                    }
-                  },
-                },
-                "<"
               );
+            // .to(
+            //   {},
+            //   {
+            //     onComplete: () => {
+            //       const contactCard = document.querySelector("#contactCard");
+            //       if (contactCard) {
+            //         contactCard.classList.add(styles.active);
+            //         // console.log(document.querySelector('#contactCard'));
+            //       }
+            //     },
+            //   },
+            //   "<"
+            // )
+            // .to(
+            //   {},
+            //   {
+            //     onComplete: () => {
+            //       const contactCard1 =
+            //         document.querySelector("#contactCard1");
+            //       if (contactCard1) {
+            //         contactCard1.classList.add(styles.active1);
+            //       }
+            //     },
+            //   },
+            //   "<"
+            // );
           }
         );
       }

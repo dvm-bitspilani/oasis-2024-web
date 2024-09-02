aeb7cca79bdbb23edabbe707a5886fc843b6c3d2
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-09-02 22:44:23 +0530

    conditional rendering slot machine for mobile ios devices

--- components/Landing/Scene/Scene.tsx
+++ components/Landing/Scene/Scene.tsx
@@ -5,9 +5,23 @@ import { Canvas, useThree } from "@react-three/fiber";
 
 import { SlotMachine2 } from "./SlotMachine2";
 import { SlotMachine } from "./SlotMachine";
-import { forwardRef } from "react";
+import { forwardRef, useEffect, useState } from "react";
 import { EffectComposer, Bloom } from "@react-three/postprocessing";
 import { SlotMachineiOS } from "./SlotMachineiOS";
+
+function detectAppleDevice() {
+  const userAgent = navigator.userAgent.toLowerCase();
+
+  if (
+    userAgent.includes("iphone") ||
+    userAgent.includes("ipad") ||
+    userAgent.includes("ipod")
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 interface Props {
   setIs3dLoaded: (value: boolean) => void;
@@ -31,6 +45,11 @@ const LandingScene = forwardRef(function LandingScene(
   }: Props,
   ref
 ) {
+  const [isAppleDevice, setIsAppleDevice] = useState<any>(null);
+
+  useEffect(() => {
+    setIsAppleDevice(detectAppleDevice());
+  }, []);
   return (
     <>
       <Canvas
@@ -64,22 +83,25 @@ const LandingScene = forwardRef(function LandingScene(
             setCamera={setCamera}
             isVideoFocused={isVideoFocused}
           /> */}
-          {/* <SlotMachine2
-            video="/Videos/curtainRaiser23_flipped.mp4"
-            ref={ref}
-            setIs3dLoaded={setIs3dLoaded}
-            iframeClick={iframeClick}
-            setCamera={setCamera}
-            isVideoFocused={isVideoFocused}
-          /> */}
-          <SlotMachineiOS
-            video="/Videos/curtainRaiser23_flipped.mp4"
-            ref={ref}
-            setIs3dLoaded={setIs3dLoaded}
-            iframeClick={iframeClick}
-            setCamera={setCamera}
-            isVideoFocused={isVideoFocused}
-          />
+          {isAppleDevice ? (
+            <SlotMachineiOS
+              video="/Videos/curtainRaiser23_flipped.mp4"
+              ref={ref}
+              setIs3dLoaded={setIs3dLoaded}
+              iframeClick={iframeClick}
+              setCamera={setCamera}
+              isVideoFocused={isVideoFocused}
+            />
+          ) : (
+            <SlotMachine2
+              video="/Videos/curtainRaiser23_flipped.mp4"
+              ref={ref}
+              setIs3dLoaded={setIs3dLoaded}
+              iframeClick={iframeClick}
+              setCamera={setCamera}
+              isVideoFocused={isVideoFocused}
+            />
+          )}
         </group>
       </Canvas>
     </>

3fd3a9c97f19ef37c2f734de32b92c0b7bd7a333
Animeshdj <animeshstick.567@gmail.com>
2024-09-07 02:23:13 +0530

    feat: add hamburg animation

--- components/Landing/Navbar/HamBtn/Btn.tsx
+++ components/Landing/Navbar/HamBtn/Btn.tsx
@@ -1,14 +1,64 @@
-import styles from './btn.module.scss'
-import MenuLines from './MenuLines'
-import HamPokerChip from './PokerChip'
+"use client";
+import { useRef, useState } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import styles from "./btn.module.scss";
+import HamPokerChip from "./PokerChip";
+import MenuLines from "./MenuLines";
 
 export default function HamBtn() {
-    return (
-        <div className={styles.btn}>
-            <HamPokerChip svgClass={styles.pokerChip} />
-            <div className={styles.hamIcon}>
-                <MenuLines svgClass={styles.menuLines} />
-            </div>
-        </div>
-    )
+  const [isHamOpen, setIsHamOpen] = useState(false);
+  const BtnRef = useRef(null);
+  const pokerChipRef = useRef(null);
+  const handleClick = () => {
+    setIsHamOpen(!isHamOpen);
+    animation();
+  };
+
+  const animation = () => {
+    const tl = gsap.timeline();
+    if (isHamOpen) {
+      tl.to(BtnRef.current, {
+        y: "0",
+        duration: 0.5,
+        scale: 1,
+      }).to(
+        pokerChipRef.current,
+        {
+          rotate: 0,
+          duration: 0.5,
+          ease: "none",
+        },
+        "<"
+      );
+    } else {
+      tl.to(BtnRef.current, {
+        y: `${window.innerHeight / 2 - 50}`,
+        duration: 0.5,
+        scale: 8.9,
+      }).to(
+        pokerChipRef.current,
+        {
+          rotate: 180,
+          duration: 1,
+          ease: "none",
+        },
+        "<"
+      );
+    }
+  };
+
+  return (
+    <div className={styles.btn} onClick={handleClick} ref={BtnRef}>
+      {isHamOpen && <div className={styles.hamOverlay} />}
+      <HamPokerChip
+        ref={pokerChipRef}
+        svgClass={styles.pokerChip}
+        isHamOpen={isHamOpen}
+      />
+      <div className={styles.hamIcon}>
+        <MenuLines svgClass={styles.menuLines} />
+      </div>
+    </div>
+  );
 }
---
+++ components/Landing/Navbar/HamBtn/MenuLines.tsx
@@ -0,0 +1,24 @@
+interface MenuLinesProps {
+  svgClass: string;
+}
+
+const MenuLines: React.FC<MenuLinesProps> = ({ svgClass }) => {
+  return (
+    <svg
+      className={svgClass}
+      viewBox="0 0 19 13"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1 6.4803H17.4409M1 1H17.4409M1 11.9606H11.9606"
+        stroke="#EDCF89"
+        strokeWidth="1.82699"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default MenuLines;

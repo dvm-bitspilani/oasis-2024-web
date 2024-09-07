f44c979314b3b733fdf2f49e5323a03bc73202d5
Samyak Jain <jainsamyak1203@gmail.com>
2024-09-07 23:17:32 +0530

    feat: add click effect

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
 import styles from "./nav.module.scss";
 import Image from "next/image";
 import about from "../../public/about.png";
@@ -7,36 +8,46 @@ import contact from "../../public/contact.png";
 import events from "../../public/events.png";
 
 const Nav = () => {
+  const [active, setActive] = useState("home");
+
+  const handleImageClick = (name) => {
+    setActive(name); 
+  };
+
   return (
     <>
       <div className={styles.wrapper}>
-      <Image
+        <Image
           src={home}
           alt="home"
           width={347}
           height={168}
-          className={styles.home}
+          className={`${styles.home} ${active === "home" ? styles.active : ""}`}
+          onClick={() => handleImageClick("home")}
         />
         <Image
           src={about}
           alt="about"
           width={347}
           height={168}
-          className={styles.about}
+          className={`${styles.about} ${active === "about" ? styles.active : ""}`} 
+          onClick={() => handleImageClick("about")}
         />
         <Image
           src={events}
           alt="events"
           width={347}
           height={168}
-          className={styles.events}
+          className={`${styles.events} ${active === "events" ? styles.active : ""}`}
+          onClick={() => handleImageClick("events")}
         />
         <Image
           src={contact}
           alt="contact"
           width={347}
           height={168}
-          className={styles.contact}
+          className={`${styles.contact} ${active === "contact" ? styles.active : ""}`}
+          onClick={() => handleImageClick("contact")}
         />
       </div>
     </>

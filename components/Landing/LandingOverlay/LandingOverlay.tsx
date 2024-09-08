0af43d4fba2fd9ae27e69781116f8ecafe75f4ad
Chaitanya Rahul Jain <175200580+chaitanya-rahul-jain@users.noreply.github.com>
2024-09-09 00:34:07 +0530

    feat: add dummy card

--- components/Landing/LandingOverlay/LandingOverlay.tsx
+++ components/Landing/LandingOverlay/LandingOverlay.tsx
@@ -1,13 +1,17 @@
+"use client";
 import styles from "./overlay.module.scss";
-
 import Navbar from "../Navbar/Navbar";
 import CardsGroup from "../CardsGroup/CardsGroup";
 import AboutUsPage from "@/components/AboutUs/AboutUsPage";
 import ContactUs from "@/components/ContactUs/ContactUs";
 import Social from "../Social/Social";
 import Countdown from "../Countdown/Countdown";
+import Card from "../Card/Card";
+import { useRef } from "react";
 
 export default function LandingOverlay() {
+  const leftCardContainerRef = useRef(null);
+  const rightCardContainerRef = useRef(null);
   return (
     <div className={styles.mainwrapper} id="mainwrapper">
       <Social />
@@ -15,18 +19,16 @@ export default function LandingOverlay() {
       <div className={styles.headerContainer}>
         <Navbar />
       </div>
-      <div className={styles.machinecontainer}>
-        {/* <div className={styles.cards} id="leftcards">
-          <CardsGroup group={1} />
-        </div>
-        <div className={styles.cards} id="rightcards">
-          <CardsGroup group={2} />
-        </div> */}
-      </div>
+      <div className={styles.machinecontainer}></div>
       <div className={styles.cardContainer}>
         <div className={styles.cardsLeftOuterContainer}>
-          <div className={styles.cardsFollowZone}></div>
-          <div className={styles.cardContainer}></div>
+          <div
+            className={styles.cardsFollowZone}
+            ref={leftCardContainerRef}
+          ></div>
+          <div className={styles.cardContainer}>
+            <div className={styles.card}></div>
+          </div>
         </div>
         <div className={styles.cardsRightOuterContainer}>
           <div className={styles.cardsFollowZone}></div>

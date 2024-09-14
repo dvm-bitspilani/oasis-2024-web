5873b25fab02e5e641366621c10aeae5008df549
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-09-14 23:58:53 +0530

    fix: removed placeholder text

--- components/Landing/Backdrop/Backdrop.tsx
+++ components/Landing/Backdrop/Backdrop.tsx
@@ -1,9 +1,5 @@
 import styles from "./backdrops.module.scss";
 
 export default function SuitBackground() {
-  return (
-    <div className={styles.backdrop}>
-      <h1>Lmao</h1>
-    </div>
-  );
+  return <div className={styles.backdrop}>{/* <h1>Lmao</h1> */}</div>;
 }

b4e717410f36f9761100acbfc1ef98fb81dc22d6
Chaitanya Rahul Jain <175200580+chaitanya-rahul-jain@users.noreply.github.com>
2024-09-14 22:52:03 +0530

    feat: div follow mouse

--- components/Landing/LandingOverlay/LandingOverlay.tsx
+++ components/Landing/LandingOverlay/LandingOverlay.tsx
@@ -8,13 +8,42 @@ import Social from "../Social/Social";
 import Countdown from "../Countdown/Countdown";
 import Nav from "@/components/Nav/Nav";
 import Card from "../Card/Card";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+
+import { useSpring, animated } from "@react-spring/web";
 
 export default function LandingOverlay() {
+  const [mouse, setMouse] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  // This will handle the spring physics for smooth animation
+  const [style, api] = useSpring(() => ({
+    x: mouse.x,
+    y: mouse.y,
+    config: { tension: 200, friction: 20 }, // Adjust the physics here for more/less resistance and stiffness
+  }));
+
+  // Update the spring whenever the mouse position changes
+  useEffect(() => {
+    api.start({
+      x: mouse.x,
+      y: mouse.y,
+    });
+  }, [mouse, api]);
+
   const leftCardContainerRef = useRef(null);
   const rightCardContainerRef = useRef(null);
   return (
-    <div className={styles.mainwrapper} id="mainwrapper">
+    <div
+      className={styles.mainwrapper}
+      id="mainwrapper"
+      onMouseMove={(e) => {
+        console.log(e.clientX, e.clientY);
+        setMouse({ x: e.clientX, y: e.clientY });
+      }}
+    >
       <Social />
       <Countdown dateString="October 23, 2024 19:00:00" />
       <div className={styles.headerContainer}>
@@ -34,7 +63,18 @@ export default function LandingOverlay() {
             }}
           ></div>
           <div className={styles.cardContainer}>
-            <div className={styles.card}></div>
+            <animated.div
+              className={styles.card}
+              style={{
+                position: "absolute",
+                left: style.x,
+                top: style.y,
+                width: "50px",
+                height: "50px",
+                backgroundColor: "red",
+                transform: "translate(-50%, -50%)",
+              }}
+            ></animated.div>
           </div>
         </div>
         <div className={styles.cardsRightOuterContainer}>

bf3cee0d8ed31f93d4d37d62294690827f7bd81f
Manas <manasck14@gmail.com>
2024-09-15 19:36:51 +0530

    feat: added events page

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,7 +1,23 @@
+import React from "react";
+import styles from "./events.module.scss"
+import Folder from "@/components/Events/Folder/Folder";
+
 export default function EventsPage() {
   return (
-    <div>
-      <h1>Events</h1>
-    </div>
+    <>
+      <div className={styles.heading}>
+        <h2>EVENTS</h2>
+      </div>
+      <div className={styles.folderContainer} >
+        <Folder name="Dance" />
+        <Folder name="Music" />
+        <Folder name="Art" />
+        <Folder name="Photography" />
+        <Folder name="Kernel" />
+        <Folder name="Fashion" />
+        <Folder name="ProfShow" />
+        <Folder name="Lorem" />
+      </div>
+    </>
   );
 }

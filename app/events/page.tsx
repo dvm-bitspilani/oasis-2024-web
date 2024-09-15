d6d6e309137a05f6be8a4d5d24aa9a013fb1efc7
Manas <manasck14@gmail.com>
2024-09-16 02:41:06 +0530

    feat: added modal for diff categories

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,23 +1,49 @@
-import React from "react";
+"use client"
+
+import React, { useState } from "react";
 import styles from "./events.module.scss"
 import Folder from "@/components/Events/Folder/Folder";
+import Category from "@/components/Events/Category/Category";
 
 export default function EventsPage() {
+
+  const [category, setCategory] = useState<string>("");
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  function handleSelect(selectedCategory: string) {
+    setCategory(selectedCategory);
+    setIsModalOpen(true);
+  }
+
+  function handleCloseModal() {
+    setIsModalOpen(false); 
+    setCategory("");
+  }
+
   return (
     <>
       <div className={styles.heading}>
         <h2>EVENTS</h2>
       </div>
       <div className={styles.folderContainer} >
-        <Folder name="Dance" />
-        <Folder name="Music" />
-        <Folder name="Art" />
-        <Folder name="Photography" />
-        <Folder name="Kernel" />
-        <Folder name="Fashion" />
-        <Folder name="ProfShow" />
-        <Folder name="Lorem" />
+        <Folder isSelected={category === 'dance'} onSelect={() => handleSelect('dance')}>Dance</Folder>
+        <Folder isSelected={category === 'music'} onSelect={() => handleSelect('music')}>Music</Folder>
+        <Folder isSelected={category === 'art'} onSelect={() => handleSelect('art')}>Art</Folder>
+        <Folder isSelected={category === 'photography'} onSelect={() => handleSelect('photography')}>Photography</Folder>
+        <Folder isSelected={category === 'kernel'} onSelect={() => handleSelect('kernel')}>Kernel</Folder>
+        <Folder isSelected={category === 'fashion'} onSelect={() => handleSelect('fashion')}>Fashion</Folder>
+        <Folder isSelected={category === 'profshow'} onSelect={() => handleSelect('profshow')}>ProfShow</Folder>
+        <Folder isSelected={category === 'lorem'} onSelect={() => handleSelect('lorem')}>Lorem</Folder>
       </div>
+      <div className={styles.category}>
+        <p>Please select a category</p>
+      </div>
+
+      {isModalOpen && (
+        <div className={styles.fixedCategoryContainer}>
+          <Category onClose={handleCloseModal} />
+        </div>
+      )}
     </>
   );
 }
--- components/Events/Folder/Folder.tsx
+++ components/Events/Folder/Folder.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import styles from "./folder.module.scss";
+import { ReactNode } from "react";
 
 interface FolderProps {
-    name: string,
+    children: ReactNode;
+    onSelect: () => void;
+    isSelected: boolean;
 }
 
 const Folder: React.FC<FolderProps> = ({
-    name,
+    children,
+    onSelect,
+    isSelected,
   }) => {
 
     return (
-        <div className={styles.folder}>
+        <button className={`${styles.folder} ${isSelected ? styles.active : ""}`}onClick={onSelect}>
             <svg width="162" height="105" viewBox="0 0 162 105" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <g filter="url(#filter0_d_3810_9076)">
                     <path d="M20.6266 96.6228H8.75586V25.3438H11.5381V21.817H14.5057V18.2902H24.3362V12.1646H27.6748V8.82343H66.8111V12.3503H70.5207V18.6614H119.673V22.0026H126.165V30.9125M20.6266 96.6228H123.011V93.6529H126.165V86.9705H130.245V80.2881H133.584V76.7612H136.922V70.6357H140.632V63.7677H143.229V57.2709H146.567V47.2473H149.535V40.7505H153.245V30.9125H126.165M20.6266 96.6228V92.7248H23.5942V86.5992H27.6748V79.5456H30.2715V76.39H33.9811V69.7076H37.5052V62.8396H40.4729V56.1572H43.6261V46.5048H46.7792V40.008H50.4888V30.9125H126.165" stroke="#FFD600" stroke-width="2.596" shape-rendering="crispEdges" />
@@ -41,8 +46,8 @@ const Folder: React.FC<FolderProps> = ({
                     </filter>
                 </defs>
             </svg>
-            <p>{name}</p>
-        </div>
+            <p>{children}</p>
+        </button>
   );
 };
 

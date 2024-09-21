1b94c5193585f7856a990aaa9d760940bea2404c
Manas <manasck14@gmail.com>
2024-09-22 02:55:23 +0530

    fix: yPercent of scroll

--- components/Landing/Landing/Landing.tsx
+++ components/Landing/Landing/Landing.tsx
@@ -561,7 +561,7 @@ export default function Landing() {
                 .to(
                   "#contact-us-inner-scroll",
                   {
-                    yPercent: -95,
+                    yPercent: -91,
                     duration: 2,
                     pointerEvents: "auto",
                   },

afe22b04c813a3552a6d5bdf497203120f062892
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-10-11 23:05:17 +0530

    feat: updated logo

--- components/Landing/Navbar/Logo/Logo.tsx
+++ components/Landing/Navbar/Logo/Logo.tsx
@@ -3,14 +3,17 @@ import styles from "./logo.module.scss";
 import Image from "next/image";
 
 export default function OasisLogo() {
+    const logoHeight = 150
+    const logoAspectRatio = 671/199
+
   return (
     <div className={styles.logo}>
       <Image
       id="oasisLogo"
         src="/oasislogo.png"
         alt="oasis logo landing"
-        width={560}
-        height={205}
+        width={logoAspectRatio * logoHeight}
+        height={logoHeight}
       />
     </div>
   );

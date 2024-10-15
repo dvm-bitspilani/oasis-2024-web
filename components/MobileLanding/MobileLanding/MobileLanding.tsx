b0dbca10de93e8f17fa31c2b365688023e4f2c5b
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-10-15 22:22:41 +0530

    style: fade between landing to about us

--- components/MobileLanding/MobileLanding/MobileLanding.tsx
+++ components/MobileLanding/MobileLanding/MobileLanding.tsx
@@ -23,42 +23,43 @@ export default function MobileLanding() {
             <div className={styles.landing}>
                 <Image src={slotMachine2D} alt='2d slot machine' className={styles.slotMachine}/>
                 <MobileRegBtn/>
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="430"
-                    height="313"
-                    viewBox="0 0 390 313"
-                    fill="none"
-                    className={styles.gradient}
-                    id="gradient"
-                >
-                    <g filter="url(#filter0_f_2271_3970)">
-                        <path d="M0 123L198 170.977L390 123V321H0V123Z" fill="black"/>
-                    </g>
-                    <defs>
-                        <filter
-                            id="filter0_f_2271_3970"
-                            x="-122.9"
-                            y="0.0999985"
-                            width="635.8"
-                            height="443.8"
-                            filterUnits="userSpaceOnUse"
-                            colorInterpolationFilters="sRGB"
-                        >
-                            <feFlood floodOpacity="0" result="BackgroundImageFix"/>
-                            <feBlend
-                                mode="normal"
-                                in="SourceGraphic"
-                                in2="BackgroundImageFix"
-                                result="shape"
-                            />
-                            <feGaussianBlur
-                                stdDeviation="61.45"
-                                result="effect1_foregroundBlur_2271_3970"
-                            />
-                        </filter>
-                    </defs>
-                </svg>
+                {/*<svg*/}
+                {/*    xmlns="http://www.w3.org/2000/svg"*/}
+                {/*    width="430"*/}
+                {/*    height="313"*/}
+                {/*    viewBox="0 0 390 313"*/}
+                {/*    fill="none"*/}
+                {/*    className={styles.gradient}*/}
+                {/*    id="gradient"*/}
+                {/*>*/}
+                {/*    <g filter="url(#filter0_f_2271_3970)">*/}
+                {/*        <path d="M0 123L198 170.977L390 123V321H0V123Z" fill="black"/>*/}
+                {/*    </g>*/}
+                {/*    <defs>*/}
+                {/*        <filter*/}
+                {/*            id="filter0_f_2271_3970"*/}
+                {/*            x="-122.9"*/}
+                {/*            y="0.0999985"*/}
+                {/*            width="635.8"*/}
+                {/*            height="443.8"*/}
+                {/*            filterUnits="userSpaceOnUse"*/}
+                {/*            colorInterpolationFilters="sRGB"*/}
+                {/*        >*/}
+                {/*            <feFlood floodOpacity="0" result="BackgroundImageFix"/>*/}
+                {/*            <feBlend*/}
+                {/*                mode="normal"*/}
+                {/*                in="SourceGraphic"*/}
+                {/*                in2="BackgroundImageFix"*/}
+                {/*                result="shape"*/}
+                {/*            />*/}
+                {/*            <feGaussianBlur*/}
+                {/*                stdDeviation="61.45"*/}
+                {/*                result="effect1_foregroundBlur_2271_3970"*/}
+                {/*            />*/}
+                {/*        </filter>*/}
+                {/*    </defs>*/}
+                {/*</svg>*/}
+                <div className={styles.fade}></div>
             </div>
             <div className={styles.aboutUs}>
                 <MobileHeading text='About Us' showArrows={true}/>

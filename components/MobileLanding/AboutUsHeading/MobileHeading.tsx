3bb9fa4c2bf7c24b1422c02754689fbdec096c36
Atharv Agarwal <atharv20agarwal@gmail.com>
2024-10-16 04:25:25 +0530

    feat: prof shows section heading

--- components/MobileLanding/AboutUsHeading/MobileHeading.tsx
+++ components/MobileLanding/AboutUsHeading/MobileHeading.tsx
@@ -1,20 +1,26 @@
-import styles from './aboutUs.module.scss'
+import styles from "./aboutUs.module.scss";
 import Image from "next/image";
 
-import arrow from '@/assets/MobileLanding/AboutUsMobile/aboutUsDecor.svg'
+import arrow from "@/assets/MobileLanding/AboutUsMobile/aboutUsDecor.svg";
 
 interface Props {
-    text: string;
-    showArrows: boolean;
-    style: any
+  text: string;
+  showArrows: boolean;
+  style: any;
+  textStyles: any;
 }
 
-export default function MobileHeading({text, showArrows, style}: Props) {
-    return (
-        <h1 className={styles.heading} style={style}>
-            {showArrows ? <Image src={arrow} alt='arrow decoration'/> : null}
-            <p>{text}</p>
-            {showArrows ? <Image src={arrow} alt='arrow decoration'/> : null}
-        </h1>
-    )
+export default function MobileHeading({
+  text,
+  showArrows,
+  style,
+  textStyles,
+}: Props) {
+  return (
+    <h1 className={styles.heading} style={style}>
+      {showArrows ? <Image src={arrow} alt="arrow decoration" /> : null}
+      <p style={textStyles}>{text}</p>
+      {showArrows ? <Image src={arrow} alt="arrow decoration" /> : null}
+    </h1>
+  );
 }
